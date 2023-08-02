9457e042f81b25cc90c280d39cee61af62837089
Pavel Nacharkin <pavel.nacharkin@gmail.com>
2023-08-03 00:02:04 +0300

    a bundle of User and Pet entities

--- src/test/java/ru/itsjava/dao/UserJdbcDaoImplTest.java
+++ src/test/java/ru/itsjava/dao/UserJdbcDaoImplTest.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.dao.EmptyResultDataAccessException;
+import ru.itsjava.domain.Pet;
 import ru.itsjava.domain.User;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -15,8 +17,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class UserJdbcDaoImplTest {
     private static final String DEFAULT_NAME = "Georg";
     private static final int DEFAULT_AGE = 18;
+
     private static final long FIRST_ID = 1L;
     private static final long NEW_ID = 4L;
+    private static final Pet DEFAULT_PET = new Pet(1L,"Maine Coon");
 
     @Autowired
     private UserDao userDao;
@@ -30,18 +34,20 @@ public class UserJdbcDaoImplTest {
 
     @Test
     public void shouldHaveCorrectInsert() {
-        User expectedUser = new User(NEW_ID, DEFAULT_NAME, DEFAULT_AGE);
-        userDao.insert(expectedUser);
-        User actual = userDao.findById(NEW_ID);
+        User expectedUser = new User(DEFAULT_NAME, DEFAULT_AGE, DEFAULT_PET);
 
-        assertEquals(actual, expectedUser);
+        long idFromDB = userDao.insert(expectedUser);
+        User actualUser = userDao.findById(idFromDB);
+
+        assertAll(() -> assertEquals(actualUser.getName(), expectedUser.getName()),
+                () -> assertEquals(actualUser.getAge(), expectedUser.getAge()));
 
     }
 
 
     @Test
     public void shouldHaveCorrectUpdate() {
-        User expectedUser = new User(FIRST_ID, DEFAULT_NAME, DEFAULT_AGE);
+        User expectedUser = new User(FIRST_ID, DEFAULT_NAME, DEFAULT_AGE, DEFAULT_PET);
         userDao.update(expectedUser);
         User actual = userDao.findById(FIRST_ID);
 
@@ -52,7 +58,7 @@ public class UserJdbcDaoImplTest {
 
     @Test
     public void shouldHaveCorrectDelete() {
-        User addedUser = new User(NEW_ID, DEFAULT_NAME, DEFAULT_AGE);
+        User addedUser = new User(NEW_ID, DEFAULT_NAME, DEFAULT_AGE, DEFAULT_PET);
         userDao.insert(addedUser);
 
         assertEquals(userDao.findById(NEW_ID), addedUser);
@@ -69,7 +75,7 @@ public class UserJdbcDaoImplTest {
 
     @Test
     public void shouldHaveCorrectFindById() {
-        User expectedUser = new User(NEW_ID, DEFAULT_NAME, DEFAULT_AGE);
+        User expectedUser = new User(NEW_ID, DEFAULT_NAME, DEFAULT_AGE, DEFAULT_PET);
         userDao.insert(expectedUser);
 
 
