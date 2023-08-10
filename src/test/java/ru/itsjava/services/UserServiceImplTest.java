7a23f70a721543d04442cae4365564348e87d5a7
Pavel Nacharkin <pavel.nacharkin@gmail.com>
2023-08-11 00:19:46 +0300

    Add test Dao and Services

--- src/test/java/ru/itsjava/services/UserServiceImplTest.java
+++ src/test/java/ru/itsjava/services/UserServiceImplTest.java
@@ -1,6 +1,6 @@
 package ru.itsjava.services;
 
-import org.junit.jupiter.api.DisplayName;
+
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
